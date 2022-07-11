cb90a333cda5806985054ce379a5e11e73f48e9f
Carlos Kelvis <kekehacct@gmail.com>
2022-07-11 10:02:00 -0300

    ex085 ao ex087 resolvidos

--- curso_em_video/mundo3_estruturas_compostas/ex085-lista_com_pares_e_impares.py
+++ curso_em_video/mundo3_estruturas_compostas/ex085-lista_com_pares_e_impares.py
@@ -1,2 +1,21 @@
 # Crie um programa onde o usuário possa digitar sete valores numéricos, e cadastre-os em uma lista única que mantenha
 # separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+
+valores = [[], []]
+
+print()
+
+for c in range(0, 7):
+    valor = int(input(f'Digite o {c + 1}° valor: '))
+    
+    if valor % 2 == 0: # Par.
+        valores[0].append(valor)
+    else: # Ímpar.
+        valores[1].append(valor)
+
+valores[0].sort()
+valores[1].sort()
+
+print(f'\nValores pares: {valores[0]}')
+print(f'Valores ímpares: {valores[1]}')
+print()
--- curso_em_video/mundo3_estruturas_compostas/ex087-mais_sobre_matriz_em_python.py
+++ curso_em_video/mundo3_estruturas_compostas/ex087-mais_sobre_matriz_em_python.py
@@ -2,3 +2,38 @@
 # - A soma de todos os valores pares digitados
 # - A soma dos valores da terceira coluna
 # - O maior valor da segunda linha
+
+matriz = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+
+soma_par = soma_val = maior = 0
+
+print()
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l + 1}, {c + 1}]: '))
+
+        if matriz[l][c] % 2 == 0:
+            soma_par += matriz[l][c]
+
+print()
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}] ', end='')
+    
+    print()
+
+for l in range(0, 3):
+    soma_val += matriz[l][2]
+
+for c in range(0, 3):
+    if c == 0 or matriz[1][c] > maior:
+        maior = matriz[1][c]
+
+print(f'A soma de todos os valores pares é {soma_par}.')
+print(f'A soma dos valores da terceira coluna é {soma_val}.')
+print(f'O maior valor da segunda linha é {maior}.')
+print()
