d100d5b1e216b5aa9baf096f354da944153f8eac
Carlos Kelvis <kekehacct@gmail.com>
2022-07-13 09:21:26 -0300

    Criei o ex090 ao ex095
    Nenhum ex resolvido ainda...

---
+++ curso_em_video/mundo3_estruturas_compostas/ex094-unindo_dicionarios_e_listas.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e
+# todos os dicionários em uma lista. No final, mostre:
+# - Quantas pessoas foram cadastradas
+# - A média de idade
+# - Uma lista com as mulheres
+# - Uma lista de pessoas com idade acima da média
