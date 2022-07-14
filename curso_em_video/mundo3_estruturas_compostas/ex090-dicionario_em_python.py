ee128ec2ae80a22d92ad788efa47fb811baba3b8
Carlos Kelvis <kekehacct@gmail.com>
2022-07-14 10:36:44 -0300

    ex090 e ex091 resolvidos

--- curso_em_video/mundo3_estruturas_compostas/ex090-dicionario_em_python.py
+++ curso_em_video/mundo3_estruturas_compostas/ex090-dicionario_em_python.py
@@ -1,2 +1,23 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o
 # conteúdo da estrutura na tela.
+
+aluno = {}
+
+print()
+
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input('Média: '))
+
+if aluno['media'] >= 6:
+    aluno['situacao'] = 'Aprovado'
+elif aluno['media'] >= 5 or aluno['media'] < 6:
+    aluno['situacao'] = 'Recuperação'
+else:
+    aluno['situacao'] = 'Reprovado'
+
+print('-' * 30)
+
+for k, v in aluno.items():
+    print(f'{k} é igual a {v}')
+
+print()
--- curso_em_video/mundo3_estruturas_compostas/ex091-jogo_de_dados_em_python.py
+++ curso_em_video/mundo3_estruturas_compostas/ex091-jogo_de_dados_em_python.py
@@ -1,2 +1,30 @@
 # Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
 # dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+jogadores = {'jogador1': randint(1, 6),
+             'jogador2': randint(1, 6),
+             'jogador3': randint(1, 6),
+             'jogador4': randint(1, 6)}
+rank = []
+
+print()
+print('Jogando dados...')
+
+for k, v in jogadores.items():
+    sleep(1)
+    print(f'{k} tirou {v} no dado.')
+
+rank = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+
+sleep(1)
+print('Rankeando...')
+
+for i, v in enumerate(rank):
+    sleep(1)
+    print(f'{i + 1}º lugar: {v[0]} com {v[1]}.')
+
+print()
