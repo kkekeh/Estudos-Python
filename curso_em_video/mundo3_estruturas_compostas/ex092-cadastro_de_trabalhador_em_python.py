33f19d0eb7789ed4b5ab880630644a6be699d4b0
Carlos Kelvis <kekehacct@gmail.com>
2022-07-15 10:18:33 -0300

    ex092 e ex093 resolvidos

--- curso_em_video/mundo3_estruturas_compostas/ex092-cadastro_de_trabalhador_em_python.py
+++ curso_em_video/mundo3_estruturas_compostas/ex092-cadastro_de_trabalhador_em_python.py
@@ -1,3 +1,27 @@
 # Crie um programa que leia nome, ano de nascimento e carteira de trabalho, e cadastre-o (com idade) em um dicionário. Se
 # por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e
 # acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+
+from datetime import datetime
+
+dados = {}
+ano_atual = datetime.now().year
+
+print()
+
+dados['nome'] = str(input('Nome: ')).strip()
+ano_nasc = int(input('Ano de Nascimento: '))
+dados['idade'] = ano_atual - ano_nasc
+dados['ctps'] = int(input('Carteira de Trabalho: (0 se não possuir) '))
+
+if dados['ctps'] != 0:
+    dados['contratacao'] = int(input('Ano de Contratação: '))
+    dados['salario'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratacao'] + 35) - ano_atual)
+
+print()
+
+for k, i in dados.items():
+    print(f'{k}: {i}')
+
+print()
--- curso_em_video/mundo3_estruturas_compostas/ex093-cadastro_de_jogador_de_futebol.py
+++ curso_em_video/mundo3_estruturas_compostas/ex093-cadastro_de_jogador_de_futebol.py
@@ -1,3 +1,24 @@
 # Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e
 # quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será
 # guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+
+jogador = {}
+gols = []
+
+print()
+
+jogador['nome'] = str(input('Nome do Jogador: ')).strip()
+jogador['partidas'] = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+
+for p in range(0, jogador['partidas']):
+    gols.append(int(input(f' Quantos gols na {p + 1}º partida? ')))
+
+jogador['gols'] = gols[:]
+jogador['total'] = sum(gols)
+
+print('-' * 30)
+
+for k, i in jogador.items():
+    print(f'{k}: {i}')
+
+print('-' * 30)
