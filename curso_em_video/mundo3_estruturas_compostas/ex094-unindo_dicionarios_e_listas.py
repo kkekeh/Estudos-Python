4c054b3e577f876333008033765a07fff8512671
Carlos Kelvis <kekehacct@gmail.com>
2022-07-18 10:03:16 -0300

    ex094 e ex095 resolvidos
    Aula 19 (Dicionários) completa.

--- curso_em_video/mundo3_estruturas_compostas/ex094-unindo_dicionarios_e_listas.py
+++ curso_em_video/mundo3_estruturas_compostas/ex094-unindo_dicionarios_e_listas.py
@@ -4,3 +4,63 @@
 # - A média de idade
 # - Uma lista com as mulheres
 # - Uma lista de pessoas com idade acima da média
+
+pessoas = []
+pessoa = {}
+soma = media = 0
+
+print()
+
+while True:
+    pessoa.clear()
+    
+    pessoa['nome'] = str(input('Nome: ')).strip()
+    
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    
+        if pessoa['sexo'] in 'MF':
+            break
+        
+        print('ERRO! Informe apenas M ou F...')
+    
+    pessoa['idade'] = int(input('Idade: '))    
+    soma += pessoa['idade']
+
+    pessoas.append(pessoa.copy())
+    
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+    
+        if resp in 'SN':
+            break
+        
+        print('ERRO! Informe apenas S ou N...')
+    
+    if resp == 'N':
+        break
+
+print(f'\nTotal de {len(pessoas)} pessoas cadastradas.')
+
+media = soma / len(pessoas)
+
+print(f'A média de idade é de {media:.1f}')
+print(f'Mulheres cadastradas:')
+
+for p in pessoas:
+    
+    if p['sexo'] == 'F':
+        print(f' {p["nome"]}')
+
+print(f'Pessoas com idade acima da média ({media:.1f}):')
+
+for p in pessoas:
+    
+    if p['idade'] >= media:
+        
+        for k, v in p.items():
+            print(f' {k} = {v};', end=' ')
+        
+        print()
+
+print()
--- curso_em_video/mundo3_estruturas_compostas/ex095-aprimorando_os_dicionarios.py
+++ curso_em_video/mundo3_estruturas_compostas/ex095-aprimorando_os_dicionarios.py
@@ -1,2 +1,68 @@
 # Aprimore o ex093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do
 # aproveitamento de cada jogador.
+
+time = []
+gols = []
+jogador = {}
+
+print()
+
+while True:
+    jogador.clear()
+    gols.clear()
+
+    jogador['nome'] = str(input('Nome do Jogador: ')).strip()
+    jogador['partidas'] = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    
+    for p in range(0, jogador['partidas']):
+        gols.append(int(input(f' Quantos gols na {p + 1}º partida? ')))
+
+    jogador['gols'] = gols[:]
+    jogador['total'] = sum(gols)
+
+    time.append(jogador.copy())
+
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        
+        if resp in 'SN':
+            break
+        
+        print('ERRO! Informe apenas S ou N...')
+    
+    if resp == 'N':
+        break
+
+print('=' * 54)
+print('cod ', end='')
+
+for k in jogador.keys():
+    print(f'{k:<15}', end='')
+
+print()
+
+for i, v in enumerate(time):
+    print(f'{i:<4}', end='')
+
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    
+    print()
+
+print('-' * 54)
+
+while True:
+    busca = int(input('Mostrar dados de qual jogador? [999 p/ interromper] '))
+
+    if busca == 999:
+        break
+    
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com o código {busca}...')
+    else:
+        print(f'LEVANTAMENTO DO JOGADOR {time[busca]["nome"].upper()}')
+
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'Na partida {i + 1} fez {g} gol(s).')
+    
+    print('-' * 54)
