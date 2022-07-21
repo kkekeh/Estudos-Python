4e41372df477928803b8a5cde9d22f32b83ab0ff
Carlos Kelvis <kekehacct@gmail.com>
2022-07-21 10:22:21 -0300

    ex101 e ex102 resolvidos

--- curso_em_video/mundo3_estruturas_compostas/ex101-funcoes_para_votacao.py
+++ curso_em_video/mundo3_estruturas_compostas/ex101-funcoes_para_votacao.py
@@ -1,2 +1,23 @@
 # Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma
 # pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
+
+print()
+ano_nasc = int(input('Ano de Nascimento: '))
+
+
+def voto(ano):
+    from datetime import date
+
+    ano_atual = date.today().year
+    idade = ano_atual - ano
+
+    if idade < 16:
+        return f'Com idade {idade}: Voto NEGADO'
+    elif idade >= 16 and idade < 18 or idade > 65:
+        return f'Com idade {idade}: Voto OPCIONAL'
+    else:
+        return f'Com idade {idade}: Voto OBRIGATÓRIO'
+
+
+print(voto(ano_nasc))
+print()
--- curso_em_video/mundo3_estruturas_compostas/ex102-funcao_para_fatorial.py
+++ curso_em_video/mundo3_estruturas_compostas/ex102-funcao_para_fatorial.py
@@ -1,3 +1,29 @@
 # Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a
 # calcular e outro chamado "show", que será um valor lógico (opcional) indicando se será mostrado ou não na tela o
 # processo de cálculo do fatorial.
+
+
+def fatorial(n, show=True):
+    """
+    -> Calcula o Fatorial de um número.
+    :param n: Número a ser calculado
+    :param show: Mostrar ou não a conta (opcional)
+    :return: Valor do fatorial
+    """
+    f = 1
+    
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+print()
+print(fatorial(5))
+print()
+help(fatorial)
