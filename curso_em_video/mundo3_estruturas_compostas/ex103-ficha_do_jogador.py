1a7e05c3469f5d26c585a3a00a9834c0d0553edd
Carlos Kelvis <kekehacct@gmail.com>
2022-07-21 14:13:13 -0300

    ex103 e ex104 resolvidos

--- curso_em_video/mundo3_estruturas_compostas/ex103-ficha_do_jogador.py
+++ curso_em_video/mundo3_estruturas_compostas/ex103-ficha_do_jogador.py
@@ -1,3 +1,25 @@
 # Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e
 # quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha
 # sido informado corretamente.
+
+print()
+
+jogador = str(input('Nome do jogador: '))
+gols = str(input('Número de gols: '))
+
+
+def ficha(nome='<não informado>', gol=0):
+    print(f'O jogador {nome} fez {gol} gol(s).')
+
+
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+
+if jogador == '':
+    ficha(gol=gols)
+else:
+    ficha(jogador, gols)
+
+print()
--- curso_em_video/mundo3_estruturas_compostas/ex104-validando_entradas_de_dados.py
+++ curso_em_video/mundo3_estruturas_compostas/ex104-validando_entradas_de_dados.py
@@ -1,3 +1,28 @@
 # Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante à função input() do Python, só
 # que fazendo a validação para aceitar apenas um valor numérico.
 # Ex: n = leiaInt('Digite um n: ')
+
+
+def leiaInt(msg):
+    valor = 0
+    ok = False
+
+    while True:
+        num = str(input(msg))
+
+        if num.isnumeric():
+            valor = int(num)
+            ok = True
+        else:
+            print('\033[31mERRO! Digite um número inteiro válido (não literal).\033[m')
+        
+        if ok:
+            break
+    return valor
+
+print()
+
+num = leiaInt('Digite um número: ')
+print(f'Você digitou o número {num}.')
+
+print()
