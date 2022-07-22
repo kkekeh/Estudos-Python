923b3d3d6fe69fae25724f2638ab560e18bb7bec
Carlos Kelvis <kekehacct@gmail.com>
2022-07-22 13:50:13 -0300

    ex105 e ex106 resolvidos
    Aula 21 (Funções II) completa.

--- curso_em_video/mundo3_estruturas_compostas/ex105-analisando_e_gerando_dicionarios.py
+++ curso_em_video/mundo3_estruturas_compostas/ex105-analisando_e_gerando_dicionarios.py
@@ -5,3 +5,32 @@
 # - A menor nota
 # - A média da turma
 # - A situação (opcional)
+
+
+def notas(*notas, sit=False):
+    dados = {}
+    tam = len(notas)
+
+    dados['notas'] = tam
+    dados['maior'] = max(notas)
+    dados['menor'] = min(notas)
+    dados['media'] = sum(notas) / tam
+    
+    if sit:
+        if dados['media'] < 5:
+            dados['situacao'] = 'RUIM'
+        elif dados['media'] <= 7:
+            dados['situacao'] = 'RAZOÁVEL'
+        else:
+            dados['situacao'] = 'BOA'
+    
+    return dados
+
+
+resp = notas(5, 8, 7, sit=True)
+
+print()
+print(resp)
+print()
+
+# Refazer esse exercício com mais lógica...
--- curso_em_video/mundo3_estruturas_compostas/ex106-sistema_interativo_de_ajuda.py
+++ curso_em_video/mundo3_estruturas_compostas/ex106-sistema_interativo_de_ajuda.py
@@ -1,2 +1,44 @@
 # Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai
 # aparecer. Quando o usuário digitar a palavra "FIM", o programa se encerrará. Importante: use cores.
+
+from time import sleep
+
+cores = {
+    'vermelho': '\033[0;30;41m',
+    'amarelo': '\033[0;30;43m',
+    'azul': '\033[0;30;44m',
+    'branco': '\033[7;40m',
+    'limpa': '\033[m',
+}
+
+
+def msg(txt, cor='limpa'):
+    tam = len(txt) + 6
+
+    print(cores[cor], end='')
+    print('~' * tam)
+    print(f'   {txt}   ')
+    print('~' * tam)
+    print(cores['limpa'], end='')
+    sleep(1)
+
+
+def ajuda(cmd):
+    msg(f'Acessando o manual do comando "{cmd}"...', 'azul')
+    print(cores['branco'])
+    help(cmd)
+    print(cores['limpa'], end='')
+    sleep(2)
+
+
+while True:
+    msg('SISTEMA DE AJUDA PyHELP', 'amarelo')
+
+    cmd = str(input('comando: (FIM p/ encerrar) -> ')).strip()
+
+    if cmd.upper() == 'FIM':
+        break
+    else:
+        ajuda(cmd)
+
+msg('Até logo!', 'vermelho')
